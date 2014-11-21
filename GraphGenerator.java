dd85f83fd334d539ffdce443c2a8a0c1d949660f
Bastien-Viatge <bastien.viatge@insa-lyon.fr>
2014-11-21 08:51:40 +0100

    1er commit du génrateur de triplet ! (mauvais nom de classe je sais..)

---
+++ GraphGenerator.java
@@ -0,0 +1,37 @@
+ï»¿
+import java.util.*
+
+public class GraphGenarator {
+	private const String URL = "#H4104 url";
+	private const String URI = "#H4104 uri";
+	protected String result;
+
+	public void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+	}
+	
+	public void readIn()  {
+		String tmp = "";
+		String token = "";
+		tmp = scan.nextLine();
+		while(!tmp.isEmpty()){
+			if(tmp.equals(URL)){
+				token = "url";
+			}else if(tmp.equals(URI)){
+				token = "uri";
+			}
+			
+			if(token.equals(URL) && !tmp.equals(URL)){
+				result += tmp+"\n";
+			}else if(token.equals(URI) && !tmp.equals(URI)){
+				//traitement d'une Uri
+				//création d'un triplet
+			}
+			tmp = scan.nextLine();
+		}
+	}
+	
+	
+
+
+}
